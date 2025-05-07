55471822b1bc42200087f372dbeb16c3b67b66ca
msdfernando <164974591+msdfernando@users.noreply.github.com>
2025-05-07 08:30:40 +0530

    Update TypeScript configuration to target ES5

---
+++ src/types/global.d.ts
@@ -0,0 +1,5 @@
+declare module 'simplebar-react/dist/simplebar.min.css';
+declare module 'flowbite-react' {
+  export const Flowbite: React.FC<{ theme: { theme: any }, children: React.ReactNode }>;
+  export const ThemeModeScript: React.FC;
+} 
